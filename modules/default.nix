4fc5f8e2d240481b36edad84deb6bb0503a2c705
mx6436 <mx6436@foxmail.com>
2026-02-19 22:39:08 +0800

    modules/default.nix: switch to recursive auto-import of all .nix files
    replaced manual list with mkModules helper that walks current dir and
    subdirs, using `builtins.readDir` types to recurse and collect nix modules.
    added guards to ignore non‑nix files and handle nested directories.

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,23 @@
-{
-  # Add your NixOS modules here
-  #
-  # my-module = ./my-module;
-  reliquary-archiver = ./reliquary-archiver.nix;
-}
+let
+  # Recursively walk a directory and collect nix files into an attrset
+  mkModules =
+    dir:
+    let
+      entriesWithType = builtins.readDir dir;
+      entries = builtins.attrNames entriesWithType;
+      addEntry =
+        acc: name:
+        let
+          path = builtins.toString dir + "/" + name;
+          typ = entriesWithType.${name};
+          isDir = typ == "directory";
+          isNix = typ == "regular" && builtins.match ".+\\.nix$" name != null;
+          attrName = builtins.substring 0 (builtins.stringLength name - 4) name;
+          recurse = if isDir then mkModules path else { };
+        in
+        acc // recurse // (if isNix then { ${attrName} = path; } else { });
+    in
+    builtins.foldl' addEntry { } entries;
+in
+# export all modules found under this directory (recursive)
+mkModules ./.
