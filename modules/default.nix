5be828f26ca631d839567c860e5ee0679d6cc918
mx6436 <mx6436@foxmail.com>
2026-02-19 22:24:56 +0800

    modules: add reliquary-archiver NixOS module

--- modules/default.nix
+++ modules/default.nix
@@ -2,4 +2,5 @@
   # Add your NixOS modules here
   #
   # my-module = ./my-module;
+  reliquary-archiver = ./reliquary-archiver.nix;
 }
---
+++ modules/reliquary-archiver.nix
@@ -0,0 +1,35 @@
+{
+  config,
+  lib,
+  pkgs,
+  reliquary-archiver ? pkgs.callPackage ../pkgs/reliquary-archiver/package.nix { },
+  ...
+}:
+
+let
+  cfg = config.programs.reliquary-archiver;
+in
+
+{
+  options.programs.reliquary-archiver = {
+    enable = lib.mkEnableOption "reliquary-archiver";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = reliquary-archiver;
+      defaultText = lib.literalExpression "pkgs.callPackage ../pkgs/reliquary-archiver/package.nix { }";
+      description = "The reliquary-archiver package to use.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+
+    security.wrappers.reliquary-archiver = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_net_raw+ep";
+      source = "${lib.getExe cfg.package}";
+    };
+  };
+}
