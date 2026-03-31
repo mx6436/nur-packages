7fa43994a4fa0a6b2045b8a2b6f4015db164b8c0
mx6436 <mx6436@foxmail.com>
2026-03-31 15:26:52 +0800

    maa-framework: add maa-deps to buildInputs

--- pkgs/maa-framework/package.nix
+++ pkgs/maa-framework/package.nix
@@ -2,9 +2,26 @@
   lib,
   stdenv,
   fetchurl,
+  callPackage,
   autoPatchelfHook,
   unzip,
   zlib,
+
+  maa-deps ? callPackage ../maa-deps/package.nix {
+    version = "2.12.2";
+    maaDepsHash = {
+      "x86_64-linux" = {
+        runtime = "sha256-hbyMULh/jrAq3cdsM38d19En/jrE/9QRxMsuJTGGj8s=";
+        devel = "sha256-cdZiaEys/tE3rLP64XNPzJ69flCcWKJ+amotiJ0d0xU=";
+        dbg = "sha256-KP+ObYo+PkQ8dEOSzBeJ+7uJXgP12glD+lSLDGPkHzI=";
+      };
+      "aarch64-linux" = {
+        runtime = "sha256-MQUw1dwr68xKPuAK/QxrdLbMlsXvaA8MNzWAXvMzWb8=";
+        devel = "sha256-f5syT3gkL58xnkcPyVXnaBmyCjSJ0fLkronUrnQ37KY=";
+        dbg = "sha256-4q5jS1LsbkoyjNJK/MN4Kt04GFGqVuYwGlMlWhim83g=";
+      };
+    };
+  },
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -19,6 +36,7 @@ stdenv.mkDerivation (finalAttrs: {
   ];
 
   buildInputs = [
+    maa-deps
     zlib
   ];
 
