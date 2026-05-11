fbcb1001a2a42169f97113d5e7dc4811482b468a
mx6436 <mx6436@foxmail.com>
2026-05-11 22:15:00 +0800

    nixos-modules: auto-import modules from directory

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -1,1 +1,12 @@
-{ }
+let
+  moduleNames = builtins.filter (
+    name: builtins.match ".*\\.nix$" name != null && name != "default.nix"
+  ) (builtins.attrNames (builtins.readDir ./.));
+in
+
+builtins.listToAttrs (
+  map (name: {
+    name = builtins.head (builtins.match "(.*)\\.nix$" name);
+    value = import ./${name};
+  }) moduleNames
+)
